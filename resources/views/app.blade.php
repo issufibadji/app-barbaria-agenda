b6c59ae98a6f1c2106206bacf8657c0634319c31
Issufi Badji <issufibadji@gmail.com>
2025-05-28 22:22:58 -0300

    FIXAjuste na permissão e role "add na bootstrap/app.ph na laravel 12"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,15 +7,35 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
+        <link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400;700&display=swap" rel="stylesheet" />
+        <style>
+            [x-cloak] {
+            display: none !important;
+        }
 
+        body {
+            font-family: 'Kode Mono', monospace;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        .link-active {
+            color: #000;
+            font-weight: bold;
+        }
+
+        </style>
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="Kode Mono, monospace">
         @inertia
     </body>
 </html>
+
