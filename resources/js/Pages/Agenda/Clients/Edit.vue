52bdd4242c3e286f62a04e1bf39a7d04c8e98ffd
Issufi Badji <45535344+issufibadji@users.noreply.github.com>
2025-06-09 23:34:14 -0300

    feat: migrate clients views to inertia

---
+++ resources/js/Pages/Agenda/Clients/Edit.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ClientForm from './ClientForm.vue'
+const props = defineProps({
+  client: Object,
+  users: Object
+})
+</script>
+
+<template>
+  <ClientForm mode="edit" :client="client" :users="users" />
+</template>
