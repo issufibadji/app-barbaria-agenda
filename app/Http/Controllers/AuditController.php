15960bf52876f44fa61aff2e6c056c8e3d98ded3
Issufi Badji <issufibadji@gmail.com>
2025-06-10 01:56:45 -0300

    FIX:Ajuste na controllers

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -18,6 +18,38 @@ class AuditController extends Controller
         ]);
     }
 
+    public function index(Request $request)
+    {
+        if (!Auth::user()->can('audit-all')) {
+            Session::flash('error', 'Permissão Negada!');
+            return redirect()->back();
+        }
+        // Inicializar a consulta
+        $query = Audit::query();
+
+        // Aplicar filtro de evento, se presente
+        if ($request->filled('event')) {
+            $query->where('event', $request->input('event'));
+        }
+
+        // Aplicar filtro de user_id, se presente
+        if ($request->filled('user_id')) {
+            $query->where('user_id', $request->input('user_id'));
+        }
+
+        if ($request->filled('start_date')) {
+            $query->whereDate('created_at', '>=', $request->input('start_date'));
+        }
+
+        if ($request->filled('end_date')) {
+            $query->whereDate('created_at', '<=', $request->input('end_date'));
+        }
+
+        // Buscar os registros de auditoria com os filtros aplicados
+        $audits = $query->orderBy('created_at', 'desc')->paginate(10);
+
+        return view('audit.index', compact('audits'));
+    }
     public function show(Audit $audit)
     {
         $audit->load('user');
