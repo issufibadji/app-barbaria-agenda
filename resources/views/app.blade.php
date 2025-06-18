2e9c350dbec12fa8480f20a890b61a4dd98200bf
trf/conibj <issufi.badji@tty2000.com.br>
2025-06-18 18:05:24 -0300

    FIXAjuste icon logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'AgenderBarber') }}</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+        <link rel="icon" href="images/logotipo.png" />
         <!-- Fonts -->
         <!-- <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
